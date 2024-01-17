914cad0d9c66dfc16f6308846b63b256ed55d203
Damian Ofman <ofman.damian@gmail.com>
2024-01-17 23:15:57 +0100

    Dodaj pliki projektów.

---
+++ connect4/Check.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connect4
+{
+    public static class Check
+    {
+        public static bool FullCheck(ref int InARow, int[][] map1, int player)
+        {
+            //left to right check
+            InARow = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (ECheck(i, j, ref InARow, map1, player))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //up to down check
+            InARow = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (SCheck(i, j, ref InARow, map1, player))
+                    {
+                        return true;
+                    }
+                }
+            }
+            //top left to down right
+            InARow = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (SECheck(i, j, ref InARow, map1, player))
+                    {
+                        return true;
+                    }
+                }
+            }
+            //top right to down left
+            InARow = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 3; j < 7; j++)
+                {
+                    //map1[i][j] = 5;
+
+                    if (SWCheck(i, j, ref InARow, map1, player))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+
+            //for testing
+            //for (int i = 3; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //    if (map1[i][j]==0)
+            //        {
+
+            //            map1[i][j] = 3;
+            //        }
+            //    }
+
+        }  
+
+        public static bool ECheck(int x, int y, ref int InARow, int[][] map1, int player)
+        {
+            
+            if (x >= 0 && x <= 6)
+            {
+                if (map1[y][x] == player)
+                {
+
+
+                    ECheck(x + 1, y, ref InARow, map1, player);
+                    InARow += 1;
+                    //Console.WriteLine(InARow);
+                    if (InARow == 4)
+                    {
+                        Console.WriteLine($"{player} won");
+                        return true;
+                    }
+                }
+                else { InARow = 0; }
+
+            }
+            
+            return false;
+        }
+        static bool SCheck(int x, int y, ref int InARow, int[][] map1, int player)
+        {
+            //
+            if (y >= 0 && y <= 5)
+            {
+                if (map1[y][x] == player)
+                {
+
+
+                    SCheck(x, y + 1, ref InARow, map1, player);
+                    InARow += 1;
+                    //Console.WriteLine(InARow);
+                    if (InARow == 4)
+                    {
+                        Console.WriteLine($"{player} won");
+                        return true;
+                    }
+                }
+                else { InARow = 0; }
+
+            }
+            
+            return false;
+        }
+        static bool SECheck(int x, int y, ref int InARow, int[][] map1, int player)
+        {
+            //
+            if (y >= 0 && y <= 5)
+            {
+                if (map1[y][x] == player)
+                {
+
+
+                    SECheck(x + 1, y + 1, ref InARow, map1, player);
+                    InARow += 1;
+                    //Console.WriteLine(InARow);
+                    if (InARow == 4)
+                    {
+                        Console.WriteLine($"{player} won");
+                        return true;
+                    }
+                }
+                else { InARow = 0; }
+
+            }
+            return false;
+        }
+        public static bool SWCheck(int x, int y, ref int InARow, int[][] map1, int player)
+        {
+            //
+            if  (x<6)
+                //(y >= 0 && y <= 5)
+            {
+                if (map1[x][y] == player)
+                {
+                    SWCheck(x + 1, y - 1, ref InARow, map1, player);
+                    InARow += 1;
+                    //Console.WriteLine(InARow);
+                    if (InARow == 4)
+                    {
+                        Console.WriteLine($"{player} won");
+                        return true;
+                    }
+                }
+                else { InARow = 0; }
+            }
+            return false;
+            
+        }
+    }
+}
---
+++ connect4/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace connect4
+{
+    internal class Program
+    {
+       public static int[][] map1 = new int[6][];
+       
+
+        static void Main(string[] args)
+        {
+          
+            //Game initialization
+            map1[0] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            map1[1] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            map1[2] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            map1[3] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            map1[4] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            map1[5] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+
+            int currentX = 0;
+            int Turn = 1;
+            int InARow = 0;
+
+            Console.CursorVisible = false;
+            DrawArrow(currentX);
+            DrawData(map1);
+            
+
+            //Game loop
+            while (!Check.FullCheck(ref InARow, map1, 1) | !Check.FullCheck(ref InARow, map1, 2))
+            {
+                
+                
+                DrawArrow(currentX);
+                currentX = Movement(currentX, ref Turn);
+                DrawData(map1);
+                
+            }
+            Console.ReadLine();
+
+        }
+        static void DrawArrow( int XPos)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write("                 " );
+            Console.SetCursorPosition(XPos, 0);
+            Console.Write("^");
+        }
+        static int Movement(int XPos,ref int Turn)
+        {
+            var input = Console.ReadKey();
+            switch (input.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    if (XPos==0)
+                    {
+                        break;
+                    }
+                    else 
+                    {
+                        XPos -= 2;
+                        break;
+                    }
+                    
+                case ConsoleKey.RightArrow:
+                    if (XPos == 12)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        XPos += 2;
+                        break;
+                    }
+                case ConsoleKey.Enter:
+
+                    int choice = XPos / 2;
+                    int Ypos = 5;
+                Start:
+                    if (map1[Ypos][choice] != 0)
+                    {
+                        if (Ypos==0)
+                        {
+
+                            Console.WriteLine("Wrong Move!");
+                            break;
+                        } 
+                        Ypos -= 1;
+                        goto Start;
+                    }
+                    else
+                    {
+                        map1[Ypos][choice] = Turn;
+                        if (Turn == 1)
+                        {
+                            Turn = 2;
+                        }
+                        else Turn = 1;
+
+                    }
+                        
+
+                    ;
+                    break;
+                    
+                default:
+                    break;
+            }
+            return XPos;
+        }
+
+
+        static void DrawData(int[][] map )
+        {
+            Console.SetCursorPosition(0, 1);
+            
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                foreach (var item in map[i])
+                {
+                    if (item ==1 )
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(item + " ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (item == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(item + " ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(item + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static void DrawMap()
+        {
+            Console.WriteLine(
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
+        }
+
+        
+    }
+}
